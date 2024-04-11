62915d11a11ac9687e7e04591c69b0a981824c0f
Charles Parames <55605195+popokola@users.noreply.github.com>
2024-04-11 10:22:56 +0000

    Rollback Gofiber to v2

--- server/main.go
+++ server/main.go
@@ -2,12 +2,38 @@ package main
 
 import (
 	"github.com/JustGritt/Ivoire-Jobs/database"
-	"github.com/gofiber/fiber/v3"
-	"github.com/gofiber/fiber/v3/middleware/cors"
-	"github.com/gofiber/fiber/v3/middleware/logger"
+	_ "github.com/JustGritt/Ivoire-Jobs/docs"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+	jwtware "github.com/gofiber/jwt/v3"
+	"github.com/gofiber/swagger"
+	"github.com/golang-jwt/jwt/v4"
 )
 
+const (
+	apiKey = "my-super-secret-key"
+)
 
+func restricted(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	c.SendString("Welcome " + name + "!")
+	return c.Next()
+}
+
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
 func main() {
 	database.Connect()
 	app := fiber.New()
@@ -16,10 +42,26 @@ func main() {
 	app.Use(logger.New(logger.Config{
 		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
 	}))
+	app.Get("/swagger/*", swagger.HandlerDefault)
 
-	app.Get("/", func(c fiber.Ctx) error {
+	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	app.Get("/metrics", monitor.New())
+
+	app.Use(jwtware.New(jwtware.Config{
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			// Token is missing, returns with error code 400 "Missing or malformed JWT"
+			return c.Status(400).JSON(fiber.Map{
+				"message": "Missing or malformed JWT",
+			})
+		},
+		SigningKey: []byte("secret"),
+	}))
+
+	// Restricted Routes
+	app.Get("/restricted", restricted)
 
 	app.Listen(":3000")
 }
