1b0e8312cd01b1b270eb12c4d664a3911a526557
alex_root <alexdieudonne02@gmail.com>
2024-05-13 11:12:13 +0200

    tmp commit

--- front_app/lib/features/auth_mod/services/user_service.dart
+++ front_app/lib/features/auth_mod/services/user_service.dart
@@ -1,117 +1,63 @@
 import 'dart:convert';
 
+import 'package:clean_architecture/features/auth_mod/models/user.dart';
+import 'package:clean_architecture/features/auth_mod/models/user_login.dart';
 import 'package:dio/dio.dart';
 
 import '../../../config/api_endpoints.dart';
 import '../../../config/app_config.dart';
 import '../../../config/app_http.dart';
+import '../models/user_signup.dart';
 
-
-class EnqueryService {
+class UserService {
   final AppHttp _http = AppHttp(
     baseUrl: ApiEndpoint.baseUrl,
     headers: {'token': Config.token},
   );
 
-  Future<Enquery?> getAll() async {
+  Future<User?> getAll() async {
     Response res = await _http.get(ApiEndpoint.enqueries);
     if (res.statusCode == 200) {
-      Enquery enquery = Enquery.fromJson(jsonEncode(res.data));
-      return enquery;
+      User user = User.fromJson(res.data);
+      return user;
     }
     return null;
   }
 
-  Future<EnquerySingle?> getOne(int id) async {
+  Future<User?> getOne(int id) async {
     Response res = await _http.get(
       '${ApiEndpoint.enquery}/$id?populate=*',
     );
     if (res.statusCode == 200) {
-      EnquerySingle enquery = EnquerySingle.fromJson(jsonEncode(res.data));
-      return enquery;
+      User user = User.fromJson(res.data);
+      return user;
     }
     return null;
   }
 
-  Future<EnquerySingle?> create(Lead lead) async {
-    Map<String, dynamic> _data = {
-      'data': {
-        'purpose': lead.purpose!.toString(),
-        'customer_name': lead.customer_name!.toString(),
-        'customer_email': lead.customer_email!.toString(),
-        'customer_mobile': lead.customer_mobile!.toString(),
-        'source': lead.source!.toString(),
-        'status': lead.status!.toString(),
-      }
-    };
+  Future<User?> register(UserSignup userSignup) async {
+    Map<String, dynamic> _data = userSignup.toJson();
     Response res = await _http.post(
       ApiEndpoint.enquery,
       data: jsonEncode(_data),
     );
     if (res.statusCode == 200 || res.statusCode == 201) {
-      EnquerySingle enquery = EnquerySingle.fromJson(jsonEncode(res.data));
-      return enquery;
+      User user = User.fromJson(res.data);
+      return user;
     }
     return null;
   }
 
-  Future<EnquerySingle?> update(int id, Lead lead) async {
-    Map<String, dynamic> _data = {
-      'data': {
-        'purpose': lead.purpose!.toString(),
-        'customer_name': lead.customer_name!.toString(),
-        'customer_email': lead.customer_email!.toString(),
-        'customer_mobile': lead.customer_mobile!.toString(),
-        'source': lead.source!.toString(),
-        'status': lead.status!.toString(),
-      }
-    };
-    Response res = await _http.put(
-      '${ApiEndpoint.enquery}/$id',
-      data: jsonEncode(_data),
-    );
-    if (res.statusCode == 200 || res.statusCode == 201) {
-      EnquerySingle enquery = EnquerySingle.fromJson(jsonEncode(res.data));
-      return enquery;
-    }
-    return null;
-  }
-
-  Future<EnquerySingle?> addFollowup(
-    int id,
-    Lead lead,
-    List<Map<String, dynamic>> followups,
-  ) async {
-    Map<String, dynamic> _data = {
-      'data': {
-        'purpose': lead.purpose!.toString(),
-        'customer_name': lead.customer_name!.toString(),
-        'customer_email': lead.customer_email!.toString(),
-        'customer_mobile': lead.customer_mobile!.toString(),
-        'source': lead.source!.toString(),
-        'status': followups.last['status'],
-        'followup': followups,
-      }
-    };
-    Response res = await _http.put(
-      '${ApiEndpoint.enquery}/$id',
-      data: jsonEncode(_data),
-    );
-    if (res.statusCode == 200 || res.statusCode == 201) {
-      EnquerySingle enquery = EnquerySingle.fromJson(jsonEncode(res.data));
-      return enquery;
-    }
-    return null;
-  }
-
-  Future<EnquerySingle?> deleteMe(int id) async {
-    Response res = await _http.delete(
-      '${ApiEndpoint.enquery}/$id',
-    );
-    if (res.statusCode == 200 || res.statusCode == 201) {
-      EnquerySingle enquery = EnquerySingle.fromJson(jsonEncode(res.data));
-      return enquery;
-    }
-    return null;
-  }
+  // Future<User?> login(UserLogin userLogin) async {
+  //   Map<String, dynamic> _data = userLogin.toJson();
+  //   Response res = await _http.put(
+  //     '${ApiEndpoint.enquery}/$id',
+  //     data: jsonEncode(_data),
+  //   );
+  //   if (res.statusCode == 200 || res.statusCode == 201) {
+  //     EnquerySingle enquery = EnquerySingle.fromJson(jsonEncode(res.data));
+  //     return enquery;
+  //   }
+  //   return null;
+  // }
 }
