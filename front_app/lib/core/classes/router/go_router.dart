a4315e524b090159cb761b77435b2188cbe0c8ba
alex_root <alexdieudonne02@gmail.com>
2024-06-04 10:48:40 +0200

    fix app login with go router

--- front_app/lib/core/classes/router/go_router.dart
+++ front_app/lib/core/classes/router/go_router.dart
@@ -1,9 +1,12 @@
 import 'package:barassage_app/config/app_routes.dart';
+import 'package:barassage_app/core/classes/app_context.dart';
+import 'package:barassage_app/core/init_dependencies.dart';
 // import 'package:barassage_app/features/main_app/app.dart';
 // import 'package:barassage_app/features/main_app/controllers/main/home_controller.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
+  navigatorKey: serviceLocator<AppContext>().navigatorKey,
   initialLocation: '/splash',
   routes: Routes().routes,
 );
