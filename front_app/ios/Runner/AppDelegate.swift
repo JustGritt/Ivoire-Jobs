0a741dab681ba301ac5f3aa043f9047b03a80f11
alex_root <alexdieudonne02@gmail.com>
2024-06-21 00:11:25 +0200

    test

--- front_app/ios/Runner/AppDelegate.swift
+++ front_app/ios/Runner/AppDelegate.swift
@@ -8,6 +8,8 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
