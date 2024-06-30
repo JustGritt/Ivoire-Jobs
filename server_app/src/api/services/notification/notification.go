fb5f91f79bd4f41fb5163f15576661cf175b5802
Charles Parames <55605195+popokola@users.noreply.github.com>
2024-06-30 18:41:12 +0000

    hotfix fcm prod error

--- server_app/src/api/services/notification/notification.go
+++ server_app/src/api/services/notification/notification.go
@@ -3,7 +3,7 @@ package notification
 import (
 	"barassage/api/configs"
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
 
 	"firebase.google.com/go/v4/messaging"
@@ -15,12 +15,32 @@ var notif *fcm.Client
 // InitFCM initializes the FCM client
 func InitFCM() *fcm.Client {
 	cfg := configs.GetConfig().FCM
-	fmt.Println(cfg)
 	ctx := context.Background()
+	credentials := map[string]string{
+		"type":                        cfg.Type,
+		"project_id":                  cfg.ProjectId,
+		"private_key_id":              cfg.PrivateId,
+		"private_key":                 cfg.PrivateKey,
+		"client_email":                cfg.ClientEmail,
+		"client_id":                   cfg.ClientId,
+		"auth_uri":                    cfg.AuthUri,
+		"token_uri":                   cfg.TokenUri,
+		"auth_provider_x509_cert_url": cfg.AuthProviderX509CertUrl,
+		"client_x509_cert_url":        cfg.ClientX509CertUrl,
+		"universe_domain":             cfg.UniverseDomain,
+	}
+
+	// Marshal the credentials map to JSON
+	credentialsJSON, err := json.Marshal(credentials)
+	if err != nil {
+		log.Fatalf("error marshaling credentials: %v", err)
+	}
+
 	client, err := fcm.NewClient(
 		ctx,
-		fcm.WithCredentialsFile("./fcm.json"),
+		fcm.WithCredentialsJSON(credentialsJSON),
 	)
+
 	if err != nil {
 		log.Fatalf("error initializing FCM client: %v", err)
 	}
