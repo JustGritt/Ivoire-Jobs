d2e2aa3626afd62ccc8158eaf7623a1089ba7db2
alex_root <alexdieudonne02@gmail.com>
2024-07-03 06:12:09 +0200

    done the become member page

---
+++ front_app/lib/features/profile_mod/services/become_barasseur_services.dart
@@ -0,0 +1,34 @@
+import 'package:barassage_app/core/classes/app_context.dart';
+import 'package:barassage_app/core/helpers/utils_helper.dart';
+import 'package:barassage_app/core/init_dependencies.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../../../config/api_endpoints.dart';
+import '../../../config/app_http.dart';
+
+BuildContext context = serviceLocator<AppContext>().navigatorContext;
+
+class BecomeBarasseurService {
+  BecomeBarasseurService();
+  final AppHttp _http = AppHttp();
+
+  Future<void> sendRequest(String reason) async {
+    try {
+      Response res = await _http
+          .post(ApiEndpoint.becomeBarasseur, data: {"reason": reason});
+      if (res.statusCode == 201) {
+        showMyDialog(context,
+            title: 'Success',
+            content: 'Your request has been sent successfully');
+        context.pop();
+        return;
+      }
+      throw res.data['message'];
+    } catch (e) {
+      print(e);
+      showMyDialog(context, content: e.toString(), title: 'Error');
+      debugPrint(e.toString());
+    }
+  }
+}
