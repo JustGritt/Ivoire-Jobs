214224421a562486ba1488fd33c39f70ae51664d
Charles Parameswaralingam <charlesparameswaralingam@MacBook-Pro-de-Charles.local>
2024-07-07 20:31:25 +0200

    hotfix build apk and deployement front

--- front_app/lib/features/auth_mod/controllers/register_email_validation_controller.dart
+++ front_app/lib/features/auth_mod/controllers/register_email_validation_controller.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:barassage_app/features/auth_mod/screens/register_email_validation_import.dart' as cond;
+import 'package:barassage_app/features/auth_mod/screens/desktop/register_email_validation.dart';
 
 class EmailValidationController extends StatelessWidget {
   const EmailValidationController({super.key});
@@ -9,7 +9,7 @@ class EmailValidationController extends StatelessWidget {
     return Title(
       title: 'Email Validation Section',
       color: Colors.blue,
-      child: const cond.RegisterEmailValidationController(), // This will use the appropriate version based on the platform
+      child: const RegisterEmailValidation(), // This will use the appropriate version based on the platform
     );
   }
 }
