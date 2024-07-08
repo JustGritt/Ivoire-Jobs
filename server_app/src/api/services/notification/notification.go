082ee6ad1fb379c640d3e2d77d811872c4da878b
Charles Parames <55605195+popokola@users.noreply.github.com>
2024-07-08 12:23:59 +0000

    update fcm and trigger pushnotif on become member

--- server_app/src/api/services/notification/notification.go
+++ server_app/src/api/services/notification/notification.go
@@ -2,8 +2,12 @@ package notification
 
 import (
 	"barassage/api/configs"
+	"barassage/api/models/user"
+	notifRepo "barassage/api/repositories/notificationPreference"
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 
 	"firebase.google.com/go/v4/messaging"
@@ -11,6 +15,15 @@ import (
 )
 
 var notif *fcm.Client
+
+// Domain type for notification domains
+type Domain string
+
+const (
+	ServiceDomain Domain = "service"
+	BookingDomain Domain = "booking"
+	MessageDomain Domain = "message"
+)
 
 // InitFCM initializes the FCM client
 func InitFCM() *fcm.Client {
@@ -48,10 +61,72 @@ func InitFCM() *fcm.Client {
 	return notif
 }
 
-func Send(ctx context.Context, message *messaging.Message) (*messaging.BatchResponse, error) {
+func Send(ctx context.Context, data map[string]string, user *user.User, domain Domain) (*messaging.BatchResponse, error) {
 	if notif == nil {
-		log.Println("FCM client not initialized")
-		return nil, nil
+		return nil, errors.New("FCM client not initialized")
 	}
-	return notif.Send(ctx, message)
+	if user == nil {
+		return nil, errors.New("user is nil")
+	}
+	userNotif, err := notifRepo.GetByUserID(user.ID)
+	if err != nil {
+		if err.Error() == "record not found" {
+			return nil, fmt.Errorf("user notification preference not found for user: %w", err)
+		}
+		return nil, fmt.Errorf("error fetching user notification preferences: %w", err)
+	}
+	if userNotif == nil {
+		return nil, errors.New("user notification preference is nil")
+	}
+
+	// Check the user preference for notification
+	if !userNotif.PushNotification {
+		return nil, errors.New("PushNotification is false, not sending notification")
+	}
+
+	switch domain {
+	case ServiceDomain:
+		if !userNotif.ServiceNotification {
+			return nil, errors.New("ServiceNotification is false, not sending notification")
+		}
+	case BookingDomain:
+		if !userNotif.BookingNotification {
+			return nil, errors.New("BookingNotification is false, not sending notification")
+		}
+	case MessageDomain:
+		if !userNotif.MessageNotification {
+			return nil, errors.New("MessageNotification is false, not sending notification")
+		}
+	default:
+		return nil, fmt.Errorf("unknown domain: %s", domain)
+	}
+
+	//create the Tokens array
+	var tokens []string
+	for _, token := range user.PushToken {
+		tokens = append(tokens, token.Token)
+	}
+
+	message := &messaging.MulticastMessage{
+		Data:   data,
+		Tokens: tokens,
+	}
+
+	res, err := notif.SendMulticast(ctx, message)
+	if err != nil {
+		return nil, fmt.Errorf("error sending message: %w", err)
+	}
+	// Handle failed tokens
+	if res.FailureCount > 0 {
+		var failedTokens []string
+		for idx, resp := range res.Responses {
+			if !resp.Success {
+				// The order of responses corresponds to the order of the registration tokens.
+				failedTokens = append(failedTokens, tokens[idx])
+			}
+		}
+		log.Printf("List of tokens that caused failures: %v\n", failedTokens)
+	}
+
+	return res, nil
 }
