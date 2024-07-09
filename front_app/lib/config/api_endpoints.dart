e73c053cbe36e8916fdba0b053f537e7969370ed
alex_root <alexdieudonne02@gmail.com>
2024-07-09 05:09:17 +0200

    fix

--- front_app/lib/config/api_endpoints.dart
+++ front_app/lib/config/api_endpoints.dart
@@ -8,8 +8,8 @@ class ApiEndpoint {
   // News Server
 
   // Server Links
-  //static const baseUrl = 'https://api.barassage.com';
-  static const baseUrl = 'https://fantastic-space-doodle-w59vv7w5w7pfp9j-8000.app.github.dev';
+  static const baseUrl = 'https://api.barassage.com';
+  //static const baseUrl = 'https://fantastic-space-doodle-w59vv7w5w7pfp9j-8000.app.github.dev';
   static const api = '${baseUrl}/api/v1';
 
   // Enqueries Endpoints
