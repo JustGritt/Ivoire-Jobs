c28fadedbac7ec09e0b2d871a07f49c062d507a0
alex_root <alexdieudonne02@gmail.com>
2024-07-19 11:38:38 +0200

    temp commmit

--- front_app/lib/core/classes/language_provider.dart
+++ front_app/lib/core/classes/language_provider.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 class LanguageProvider extends ChangeNotifier {
-  Locale _locale = const Locale('en');
+  Locale _locale = const Locale('en', 'US');
 
   Locale get locale => _locale;
 
   void setLocale(Locale locale) {
     _locale = locale;
+    notifyListeners();
+  }
+
+  void toggleLocale() {
+    _locale = _locale.languageCode == 'en'
+        ? const Locale('fr', 'FR')
+        : const Locale('en', 'US');
     notifyListeners();
   }
 }
